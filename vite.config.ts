d6bf1d71562ae3389bf775ebfdeadd56f24f7b9e
syedatahsinajannat-creator <syedatahsinajannat@gmail.com>
2026-09-15 15:40:18 +0600

    Fix GitHub Pages base configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/assignment-5/',
 })
-
-base: "/assignment-5/",
